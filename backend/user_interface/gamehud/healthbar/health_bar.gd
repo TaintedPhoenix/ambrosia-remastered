bc0819993bc41ba954808bac70996399ed89d840
Antony Rinaldi <antonyjrinaldi@gmail.com>
2024-09-20 13:57:05 -0400

    A bit of healthbar work

--- backend/user_interface/gamehud/healthbar/health_bar.gd
+++ backend/user_interface/gamehud/healthbar/health_bar.gd
@@ -1,7 +1,7 @@
 extends Control
 
-var MaxHealth : float = 100
-var bonusHealth : float = 0
+var maxHealth : float = 100
+var bonusHealth : float = 20
 var health : float = 100
 
 var heartScene = preload("res://backend/user_interface/gamehud/healthbar/heart.tscn")
@@ -10,13 +10,12 @@ var hearts = []
 # Called when the node enters the scene tree for the first time.
 @warning_ignore("integer_division")
 func _ready() -> void:
-	for i in range(int(MaxHealth) / 10 + (1 if int(MaxHealth) % 10 != 0 else 0)):
-		var hs = heartScene.instantiate()
-		hs.position = Vector2(44*(i-1), 0)
-		hs.name = "heart" + str(i)
-		add_child(hs)
+	health = maxHealth + bonusHealth
+	pass
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	pass
+	$TextureRect.modulate = Color.from_hsv(0, 0.0, 0.0)
+	health -= 20 * delta
+	
