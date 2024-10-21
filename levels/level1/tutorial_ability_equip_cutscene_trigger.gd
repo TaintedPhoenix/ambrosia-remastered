bf2f5378d2fb48b96523d5304291635d46b68320
Antony Rinaldi <antonyjrinaldi@gmail.com>
2024-10-21 18:41:23 -0400

    Quick Bug Fix

--- levels/level1/tutorial_ability_equip_cutscene_trigger.gd
+++ levels/level1/tutorial_ability_equip_cutscene_trigger.gd
@@ -4,7 +4,7 @@ extends Node
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	GameData.itemEquipped.connect(func(item, _category):
-		if item is AbilityItem and item.id == "redshard":
+		if item is AbilityItem and item.id == "whiteshard":
 			UI.playCutscene("tutorialDoubleJump")
 			queue_free()
 		)
