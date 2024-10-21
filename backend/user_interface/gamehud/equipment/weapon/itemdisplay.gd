9da8dea54f98860e9d44946a39a831fe4cee7ba2
Antony Rinaldi <antonyjrinaldi@gmail.com>
2024-10-21 17:34:17 -0400

    Visual changes to slash animation and gamehud
    To compensate for item sprite rotation changing.
    Also fixed a bug in level1 resulting from item id changes

--- backend/user_interface/gamehud/equipment/weapon/itemdisplay.gd
+++ backend/user_interface/gamehud/equipment/weapon/itemdisplay.gd
@@ -4,13 +4,14 @@ extends Control
 @onready var ogPos = position
 
 var rotations = {
-	"sword" : 45,
-	"bloody_cleaver" : 45
+	"sword" : -45,
+	"excalibur" : -45,
+	"calamity" : -45
 }
 
 func setRotation(i : Item):
-	if i.id in rotations and rotations[i.id] == 45:
-		rotation_degrees = 45
+	if i.id in rotations and rotations[i.id] == -45:
+		rotation_degrees = -45
 		scale = (200/sqrt(2*((size.x)**2))) * ogScale
 		position = Vector2(ogScale.x * 100, 0) + ogPos
 	else:
